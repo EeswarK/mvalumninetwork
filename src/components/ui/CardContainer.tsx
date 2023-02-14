d7e0ed781e4e47537e3bee8b3ce95f2d36f91f28
Eeswar <Eeswarkartikeya@gmail.com>
2023-02-14 13:23:15 -0800

    added components
    card container
    textip
    tooltip (not sure if this acc works, will fix if necessary)

---
+++ src/components/ui/CardContainer.tsx
@@ -0,0 +1,18 @@
+interface CardContainerProps {
+  children: React.ReactNode[];
+  onClick?: () => void;
+}
+
+export const CardContainer = (props: CardContainerProps) => {
+  const [title, body, footer] = props.children;
+  return (
+    <div
+      className="cursor-pointer select-none overflow-hidden rounded-lg shadow-2xl"
+      onClick={props.onClick}
+    >
+      <div className="px-4 pt-4 sm:px-6 sm:pt-5">{title}</div>
+      <div className="px-4 py-1 sm:px-6">{body}</div>
+      <div className="px-4 pb-4 sm:px-6 sm:pb-5">{footer}</div>
+    </div>
+  );
+};
---
+++ src/components/ui/TextTip.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+
+interface TextTipProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
+  tip: string;
+  className?: string;
+}
+export default function TextTip(props: TextTipProps) {
+  const { tip, children, className } = props;
+
+  return (
+    <span className="group cursor-default">
+      {/* translate values are hard coded, we could just hard code and pass this in for every use if there's no better solution */}
+      <div className="absolute z-50  hidden -translate-y-6 -translate-x-8 scale-50 rounded-md bg-zinc-700 px-2 py-1 text-white transition delay-150 ease-in-out  group-hover:block group-hover:scale-100">
+        {tip}
+      </div>
+      <div
+        className={clsx(
+          "absolute text-violet-500 hover:cursor-pointer group-hover:text-violet-700",
+          className
+        )}
+      >
+        {children}
+      </div>
+    </span>
+  );
+}
