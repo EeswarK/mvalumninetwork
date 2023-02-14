0e4a3659b676388299a44055839906c25e219c7c
Eeswar <Eeswarkartikeya@gmail.com>
2023-02-14 13:43:21 -0800

    made home page preferred for users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NavBar } from "@/components/ui/NavBar";
-import { type NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Landing from "../components/Landing/Landing";
 
@@ -23,3 +24,20 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (session && context.req.url === "/") {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
